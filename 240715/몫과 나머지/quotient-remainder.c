0432f5362446b82334019fc526d9f4ac9258dfea
ahnsyeon <ahnseoyn@gmail.com>
2024-07-15 18:24:41 +0900

    Title: 몫과 나머지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/몫과 나머지/quotient-remainder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    scanf("%d %d", &a, &b);
+    c = a / b;
+    d = a % b;
+    printf("%d...%d", c, d);
+    return 0;
+}
